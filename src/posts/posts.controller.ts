4133068d347f9b33ff8727303ba8771778f362d7
yuuFen <603236742@qq.com>
2019-12-17 01:16:40 +0800

    增删改查框架

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,9 +1,16 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Body, Query, Param, Put, Delete } from '@nestjs/common';
 import { get } from 'http';
-import { ApiTags, ApiOperation } from '@nestjs/swagger';
+import { ApiTags, ApiOperation, ApiProperty } from '@nestjs/swagger';
+
+class CreatePostDto {
+  @ApiProperty({ description: '文章标题' })
+  title: string
+  @ApiProperty({ description: '文章内容' })
+  content: string
+}
 
 @Controller('posts')
-@ApiTags('帖子')
+@ApiTags('文章')
 export class PostsController {
 
   @Get()
@@ -13,17 +20,36 @@ export class PostsController {
   }
 
   @Post()
-  create() {
+  @ApiOperation({ summary: '创建文章' })
+  create(@Body() body: CreatePostDto) {
+
     return {
       success: true
     }
   }
 
   @Get(':id')
-  detail() {
+  @ApiOperation({ summary: '文章详情' })
+  detail(@Param('id') id: string) {
     return {
-      id: 1,
-      title: 'Hello'
+      id: id,
     }
   }
+
+  @Put(':id')
+  @ApiOperation({ summary: '编辑文章' })
+  update(@Param('id') id: string, @Body() body: CreatePostDto) {
+    return {
+      success: true
+    }
+  }
+
+  @Delete(':id')
+  @ApiOperation({ summary: '删除文章' })
+  remove(@Param('id') id: string) {
+    return {
+      success: true
+    }
+  }
+
 }
